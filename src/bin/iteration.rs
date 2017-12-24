f70251522ac6833d9dbaf6daa64e2c5cd2555f5b
Timigen <timigen@gmail.com>
2017-12-24 10:38:45 -0500

    loop over vector

--- src/bin/iteration.rs
+++ src/bin/iteration.rs
@@ -13,4 +13,22 @@ pub fn loops() {
         println!("break" );
         break;
     }
+
+    println!("\nloop over int vector [1, 10, 20]");
+    let numbers = vec![1, 10, 20];
+    for number in numbers.iter() {
+        match number {
+            & 10 => println!("ten!"),
+            _ => println!("{} is NOT ten", number),
+        }
+    }
+
+    println!("\nloop over string vector ['apple', 'banana', 'pear']");
+    let fruits = vec!["apple", "banana", "pear"];
+    for fruit in fruits.iter() {
+        match fruit {
+            & "banana" => println!("banana!"),
+            _ => println!("{} is NOT a banana", fruit),
+        }
+    }
 }
